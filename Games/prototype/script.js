d25d5604e3fcdc6532963ecd86d9b5479d797bf1
Rob Blake <robbie.a.blake@gmail.com>
2023-05-01 17:11:35 +0200

    Showing the score

--- Games/prototype/script.js
+++ Games/prototype/script.js
@@ -140,15 +140,23 @@ window.addEventListener("load", function () {
       this.game = game;
       this.fontSize = 25;
       this.fontFamily = "Helvetica";
-      this.color = "yellow";
+      this.color = "white";
     }
 
     draw(context) {
-      // ammo stats
+      context.save();
       context.fillStyle = this.color;
+      context.shadowOffsetX = 2;
+      context.shadowOffsetY = 2;
+      context.shadowColor = "black";
+      context.font = this.fontSize + "px" + this.fontFamily;
+      // score
+      context.fillText("Score: " + this.game.score, 20, 40);
+      // ammo stats
       for (let i = 0; i < this.game.ammo; i++) {
         context.fillRect(20 + 5 * i, 50, 3, 20);
       }
+      context.restore();
     }
   }
 
@@ -174,6 +182,8 @@ window.addEventListener("load", function () {
       this.enemyTimerInterval = 1000;
 
       this.gameOver = false;
+      this.score = 0;
+      this.winningScore = 10;
     }
 
     update(deltaTime) {
@@ -198,6 +208,7 @@ window.addEventListener("load", function () {
             if (enemy.lives <= 0) {
               enemy.markedForDeletion = true;
               this.score += enemy.score;
+              if (this.score > this.winningScore) this.gameOver = true;
             }
           }
         });
